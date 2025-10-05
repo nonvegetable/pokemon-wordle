1864c4971b80024ffc1fe7c020aa258d0723bb2a
nonvegetable <vpalande81203@outlook.com>
2025-10-05 21:53:26 +0530

    built structures for the apis

--- pokemon-frontend/src/components/Game.jsx
+++ pokemon-frontend/src/components/Game.jsx
@@ -1,32 +1,37 @@
-import React, {useState} from "react"
-import Hints from "./Hints"
+import React, {useState} from "react" 
+import Hints from "./Hints" 
 
-export default function Game({pokemon}){
-
-    const [pokemonGuess, setPokemonGuess] = useState("");
-
-    const hintsArray = []
-
-    function guessPokemon(){
-        alert(`You guessed ${pokemonGuess}`)
-        //if wrong, send the prop to the Hints component to dynamically show hints about that pokemon
-        let attempts;
+export default function Game({pokemon}){ 
+    const [pokemonGuess, setPokemonGuess] = useState(""); 
+    const pokemonName; //fetched from backend 
+    fetch("api/today") 
+    const hintsArray = [] //fetched from backend 
+    
+    function guessPokemon(){ 
+        alert(You guessed ${pokemonGuess}) 
+        //if wrong, send the prop to the Hints component to dynamically show hints about that pokemon 
+        let attempts = 0; 
+        
+        if(pokemonGuess != pokemonName){ 
+            attempts++; hintsArray[0] //first from the hints should be displayed 
+            fetch("/api/hints", { 
+                method: "POST", 
+                body: JSON.stringify({ attempts: attempts }) }) 
+                .then(response => { let sprite = setSprite(response.sprite) 
+                    let hints = setHints([response.hints]) 
+                }) 
+            } 
+        } 
+        
+        return( 
+            <> 
+                <h1>Welcome to Guess the Pokemon</h1>
+                <div className="game-area"> 
+                    <img src="something" alt="pokemon"/> 
+                    <input type="text" value={pokemonGuess} onChange={(e) => setPokemonGuess(e.target.value)} /> 
+                    <button className="guess-btn" onClick={guessPokemon} >Guess</button> 
+                    {/* <Hints pokemon={{pokemonName: name, no: attempts, hints: hintsArray}}/> */} 
+                </div> 
+            </> 
+        )
     }
-
-    return(
-        <>
-            <h1>Welcome to Guess the Pokemon</h1>
-            <div className="game-area">
-                <img src="something" alt="pokemon"/>
-                <input
-                    type="text"
-                    value={pokemonGuess}
-                    onChange={(e) => setPokemonGuess(e.target.value)}
-                />
-                <button className="guess-btn" onClick={guessPokemon} >Guess</button>
-                {/* <Hints pokemon={{pokemonName: name, no: attempts, hints: hintsArray}}/> */}
-            </div>
- 
-        </>
-    )
-}
